83c67d25ae8a20431d27bf753b72f32b277927cb
NikitaIon <nikitajohngalt@gmail.com>
2021-01-31 11:05:48 +0400

    Added initial Odoo files

---
+++ odoo-academy/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Academy',
+    'version': '0.1',
+    'description': """Academy module for manage Training: Courses, Sessions...""",
+    'category': 'Training',
+    'author': 'NVP',
+
+    'depends': ['base'],
+    'data': [
+        #data
+
+
+        #views
+    ],
+    'demo': [],
+}
