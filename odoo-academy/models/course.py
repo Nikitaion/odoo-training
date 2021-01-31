fd537fa16588aca9ec55ba5237e9af4aa67c20c5
NikitaIon <nikitajohngalt@gmail.com>
2021-01-31 12:39:22 +0400

    Added fields in a Course model

--- odoo-academy/models/course.py
+++ odoo-academy/models/course.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models
+from odoo import models, fields
 
 class Course(models.Model):
     _name = 'academy.course'
     _description = 'Course info'
 
+    name = fields.Char(string='Student name', required=True)
+    description = fields.String(string='About student')
+    level = fields.Selection([('beginner', 'Beginner'),('intermediate', 'Intermediate'),('professional', 'Professional')])
+    active = fields.Boolean(string='Is active', default=True)
