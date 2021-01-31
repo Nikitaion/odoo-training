97da36a247d4d179ce59a0b0c6c62c34a6b9c867
NikitaIon <nikitajohngalt@gmail.com>
2021-01-31 15:43:51 +0400

    Added form and tree view

--- odoo-academy/models/course.py
+++ odoo-academy/models/course.py
@@ -5,7 +5,6 @@ from odoo import models, fields
 class Course(models.Model):
     _name = 'academy.course'
     _description = 'Course info'
-    _inherit = ['mail.thread']
 
     name = fields.Char(string='Title', required=True)
     description = fields.Text(string='Description')
