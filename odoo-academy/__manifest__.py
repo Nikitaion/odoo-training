83c22098709dc6291ff80dfcb2c818901e5c8465
NikitaIon <nikitajohngalt@gmail.com>
2021-01-31 20:06:30 +0400

    Added strings in manifest

--- odoo-academy/__manifest__.py
+++ odoo-academy/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Odoo Academy',
-    'version': '0.22',
+    'version': '0.23',
     'description': """Academy module for manage Training: Courses, Sessions...""",
     'category': 'Training',
     'author': 'NVP',
@@ -11,8 +11,11 @@
         #data
         'views/menu.xml'
 
-
+        #'security/ir.model.access.csv'
         #views
     ],
     'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
 }
