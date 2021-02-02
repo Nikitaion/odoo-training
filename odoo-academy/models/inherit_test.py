077b66f55969c8810442b7982b5c98f2e56822bb
NikitaIon <nikitajohngalt@gmail.com>
2021-02-02 12:17:36 +0400

    Added model and view for inherit test

--- odoo-academy/models/inherit_test.py
+++ odoo-academy/models/inherit_test.py
@@ -3,4 +3,5 @@ from odoo import models, fields, api
 class InheritTest(models.Model):
     _inherit = 'res.partner'
 
+    another_test_field = fields.Many2many(string='Hello! I am a new field!')
     test_field = fields.Many2many(comodel_name='academy.course', string='Inherit field')
