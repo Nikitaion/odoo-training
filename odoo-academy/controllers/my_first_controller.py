77137b86f22dba6dd8b7b36170bd43a86a8eb5ba
NikitaIon <nikitajohngalt@gmail.com>
2021-02-03 09:38:28 +0400

    added controller

--- odoo-academy/controllers/my_first_controller.py
+++ odoo-academy/controllers/my_first_controller.py
@@ -1,8 +1,8 @@
 from odoo import http
 from odoo.http import request
 
-class MyController(odoo.http.Controller):
-    @route('/myurl', auth='public')
+class MyController(http.Controller):
+    @http.route('/myurl', auth='public')
     def my_controller(self):
         my_scope = {'text':'hell to world!'}
-        return request.render('views/qweb_template_for_controller.xml', my_scope)
+        return request.render('controller_template', my_scope)
