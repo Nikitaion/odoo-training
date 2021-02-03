e24f9f650c06d0a15683833a725440703618240c
NikitaIon <nikitajohngalt@gmail.com>
2021-02-03 09:40:33 +0400

    added controller

--- odoo-academy/controllers/my_first_controller.py
+++ odoo-academy/controllers/my_first_controller.py
@@ -2,7 +2,7 @@ from odoo import http
 from odoo.http import request
 
 class MyController(http.Controller):
-    @http.route('/myurl', auth='public')
+    @http.route('/myurl', auth='public', website=True)
     def my_controller(self):
         my_scope = {'text':'hell to world!'}
         return request.render('controller_template', my_scope)
